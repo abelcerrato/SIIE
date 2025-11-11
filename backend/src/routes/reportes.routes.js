d03b554ba6bb7cb91c23ad7f4a2ecc1741b8c9fb
Seydi-Lara <seydi.lara@coned.gob.hn>
2025-11-11 11:05:07 -0600

    Reportes DES y Unidades Infop

--- backend/src/routes/reportes.routes.js
+++ backend/src/routes/reportes.routes.js
@@ -58,6 +58,8 @@ import {
     getconeanfo_atencionesC,
     getconeanfo_participantesC,
     getconeanfo_atenciones_prticipantes_proceso_educativosC,
+    getInfop_capacitados_por_unidades_y_cursosInfop_regionesC,
+    getDES_matricula_2024C,
 } from "../controllers/reportes.controller.js";
 
 const router = Router();
@@ -94,12 +96,15 @@ router.get("/infopcapacitadosporprogramas", getInfop_capacitados_por_programasC)
 router.get("/infopcapacitadosporegional", getInfop_capacitados_por_regionalC);
 router.get("/infopcapacitadosporsectoreconomico", getInfop_capacitados_por_sector_economicoC);
 router.get("/infopregiones", getInfop_regionesC);
+router.get("/infopunidadesycursos", getInfop_capacitados_por_unidades_y_cursosInfop_regionesC);
 
 
 /* DES */
 router.get("/desmatriculaporgradoacademico", getDES_matricula_por_grado_academicoC);
 router.get("/desmatriculaporinstitucion", getDES_matricula_por_institucionC);
 router.get("/desmatriculaporsexo", getDES_matricula_por_sexoC);
+router.get("/desmatricula2024", getDES_matricula_2024C); //matricula del año 2024
+
 
 
 /* SIIE */
